c5485cd8fe4b05f7a0b38ff38f11f3bff869a991
huangfuyichuan <huangfuyichuan@163.com>
2024-04-09 21:08:57 +0800

    【新增】新增加载静态库编译

---
+++ product/common/testfunc2/inc/testFunc2.h
@@ -0,0 +1,10 @@
+/*
+** testFunc.h
+*/
+
+#ifndef _TEST_FUNC2_H_
+#define _TEST_FUNC2_H_
+
+void func2(int data);
+
+#endif
---
+++ product/common/testfunc2/src/testFunc2.c
@@ -0,0 +1,11 @@
+/*
+** testFunc.c
+*/
+
+#include <stdio.h>
+#include "testFunc2.h"
+
+void func2(int data)
+{
+	printf("data2 is %d\n", data);
+}
